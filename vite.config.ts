a33c78e11c57460d77904a448b54c9e903f8f46c
Ayushi1222 <ayushikatroliya17h@gmail.com>
2025-09-14 00:18:28 +0530

    host

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig(({ mode }) => {
         preview: {
             host: '0.0.0.0',
             port: Number(process.env.PORT) || 4173,
-            allowedHosts: ['planit-4u0a.onrender.com']
+            allowedHosts: 'all'
         }
     };
 });
