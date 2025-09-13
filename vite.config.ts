f6b1f42e9009b4b7eb0529f1bce223b54f78b373
Ayushi1222 <ayushikatroliya17h@gmail.com>
2025-09-14 00:22:03 +0530

    host

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig(({ mode }) => {
         preview: {
             host: '0.0.0.0',
             port: Number(process.env.PORT) || 4173,
-            allowedHosts: 'all'
+            allowedHosts: ['*']
         }
     };
 });
